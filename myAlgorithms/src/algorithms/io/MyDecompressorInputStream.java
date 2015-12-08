bc1b7e7c686edc4f45a2659ff37245ec1baa3028
AsafKaravani <asaf.karavani1@gmail.com>
2015-12-08 16:12:03 +0200

    update

--- myAlgorithms/src/algorithms/io/MyDecompressorInputStream.java
+++ myAlgorithms/src/algorithms/io/MyDecompressorInputStream.java
@@ -1,12 +1,19 @@
 package algorithms.io;
 
+import java.io.IOException;
 import java.io.InputStream;
 
-public class MyDecompressorInputStream {
+public class MyDecompressorInputStream extends InputStream{
 	InputStream in;
 	
 	
 	public MyDecompressorInputStream(InputStream in){
 		this.in=in;
 	}
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
